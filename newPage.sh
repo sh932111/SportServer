593d3e53aad0f6f896bea926e0263cacfbee9e87
yojia <sh93211@gmail.com>
2014-11-17 23:45:55 +0800

    add LeafletPage

--- newPage.sh
+++ newPage.sh
@@ -6,12 +6,12 @@ read -p "File Name : " file
 test -z $file && exit 0
 
 if [[ ! -e $folder ]]; then
-mkdir $folder && cd $folder && mkdir $file && cd $file && touch "$file.html" && touch "$file.css" && touch "$file.js" && echo "<div>\n\t<script type='text/javascript' src='$folder/$file/$file.js'></script>\n\t<link rel='stylesheet' href='$folder/$file/$file.css'>\n<div>"  >> "$file.html"
+mkdir $folder && cd $folder && mkdir $file && cd $file && touch "$file.html" && touch "$file.css" && touch "$file.js" && echo "<div>\n\t<script type='text/javascript' src='$folder/$file/$file.js'></script>\n\t<link rel='stylesheet' href='$folder/$file/$file.css'>\n</div>"  >> "$file.html"
 else
     cd $folder
     if [[ ! -e $file ]]; then
         mkdir $file && cd $file && touch "$file.html" && touch "$file.css" && touch "$file.js" && echo "<div>\n\t<script type='text/javascript' src='$folder/$file/$file.js'></script>\n\t<link rel='stylesheet' href='$folder/$file/$file.css'>\n</div>"  >> "$file.html"
-    else
+    elses
         echo "mkdir: unary operator expected"
     fi
 fi
