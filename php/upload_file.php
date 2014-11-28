eb61d741a31cc389dcc510b91016d4fbed15e678
yojia <sh93211@gmail.com>
2014-11-28 17:30:17 +0800

    utf-8 bug

--- php/upload_file.php
+++ php/upload_file.php
@@ -12,7 +12,7 @@ mkdir($target_path);
 $target_path2  = $target_path."file"."/";//接收文件目录  
 mkdir($target_path2);
 
-$target_path3 = $target_path2 . basename(mb_convert_encoding($_FILES["file"]["name"], "big5", "utf8" ));  
+$target_path3 = $target_path2 . basename( $_FILES['file']['name']);  
 
 if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path3)) {  
     echo "The file ".  basename( $_FILES['file']['name']). " has been uploaded";  
